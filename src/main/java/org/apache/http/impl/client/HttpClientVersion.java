2e5dcf6ccd49661dc78c95515b37ed1c9ffaf662
veltrup <veltrup@sitepark.com>
2021-01-12 07:04:35 +0100

    update httpclient dependency

--- src/main/java/org/apache/http/impl/client/HttpClientVersion.java
+++ src/main/java/org/apache/http/impl/client/HttpClientVersion.java
@@ -2,10 +2,10 @@ package org.apache.http.impl.client;
 
 public class HttpClientVersion {
 
-  private HttpClientVersion() {
-    // do nothing
-  }
+private HttpClientVersion() {
+	// do nothing
+}
 
-  public static final String DEFAULT_USER_AGENT = HttpClientBuilder.DEFAULT_USER_AGENT;
+public static final String DEFAULT_USER_AGENT = "Apache-HttpClient";
 
 }
